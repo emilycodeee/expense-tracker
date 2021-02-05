52314e1e789148da418c6615216bc54fbb953d22
emily_chen <emilycodeee@gmail.com>
2021-02-05 23:58:34 +0800

    feat:add hoamepage category list & total amount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,21 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 const iconGenerator = require('../../Icongenerator')
 
 // home page
 router.get('/', (req, res) => {
+  let totalAmount = 0
 
-  return Record.find()
+  const categoryItem = []
+  Category.find()
     .lean()
-    .then(records => {
-      records.forEach(record => {
-        record.icon = iconGenerator(record.category)
-        console.log(record.category)
-      })
-      res.render('index', { records })
+    .then((item) => {
+      categoryItem.push(...item)
+      console.log(categoryItem)
+
+      Record.find()
+        .lean()
+        .then(records => {
+          records.forEach(record => {
+            totalAmount += record.amount
+            record.icon = iconGenerator(record.category)
+            console.log(record.category)
+          })
+          res.render('index', { records, totalAmount, categoryItem })
+        })
+        .catch(error => console.log(error))
     })
     .catch(error => console.log(error))
 })
-
 module.exports = router
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,4 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+
+
 module.exports = router
