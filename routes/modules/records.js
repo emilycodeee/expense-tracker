8224be818e2fa8f07d10f2a7c7ac8dead83a334a
emily_chen <emilycodeee@gmail.com>
2021-02-06 22:27:47 +0800

    feat:add new.hbs

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,9 +3,24 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
+// get new page
 router.get('/new', (req, res) => {
-  return res.render('new')
+  return Category.find()
+    .lean()
+    .then(categoryItem => res.render('new', { categoryItem }))
+    .catch(error => console.log(error))
 })
+
+// post new data
+
+router.post('/', (req, res) => {
+  const record = req.body
+  console.log(record)
+  return Record.create(record)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 
 module.exports = router
