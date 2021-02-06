317571ceffbaa87c3656912dc263579fe35cd8f7
emily_chen <emilycodeee@gmail.com>
2021-02-07 00:07:53 +0800

    feat:add delete function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -56,5 +56,15 @@ router.post('/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// delete 
+
+router.post('/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 module.exports = router
