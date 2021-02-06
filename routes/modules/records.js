dfded042104846b605d495d3e09a6b594dfab3e4
emily_chen <emilycodeee@gmail.com>
2021-02-06 23:06:46 +0800

    feat:add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
-// get new page
+// get new page & renderCategory
 router.get('/new', (req, res) => {
   return Category.find()
     .lean()
@@ -21,6 +21,24 @@ router.post('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+// get edit page
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  const categoryItem = []
+  Category.find()
+    .lean()
+    .then((item) => {
+      categoryItem.push(...item)
+
+      return Record.findById(id)
+        .lean()
+        .then(record => res.render('edit', { record, categoryItem }))
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error))
+})
 
 
 module.exports = router
