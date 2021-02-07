e32aa2326fc6bd9dc2bc02b5031f0a37f7eb1c73
emily_chen <emilycodeee@gmail.com>
2021-02-07 15:10:55 +0800

    fix:renew seeder function

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -5,44 +5,14 @@ const categoryList = require('./category.json').results
 db.once('open', () => {
   const categories = []
   categoryList.forEach(categoryItem => {
-    categories.push(
-      Category.create({
-        name: categoryItem.name,
-        name_en: categoryItem.name_en,
-        icon: categoryItem.icon
-      })
-    )
+    categories.push(categoryItem)
   })
-  console.log('done creating categoryseeder!')
-  Promise.all(categories)
-    .catch(error => console.log(error))
-    .finally(() => db.close())
+  Category.create(categories)
+    .then(() => {
+      console.log('insert data done...')
+      return db.close()
+    }).then(() => {
+      console.log('done creating categoryseeder!')
+    })
 })
 
-// db.once('open', () => {
-//   const categories = []
-//   categoryList.forEach(categoryItem => {
-//     categories.push(categoryItem)
-//   })
-//   Category.create(categories)
-//     .then(() => {
-//       console.log('insert data done...')
-//       return db.close()
-//     }).then(() => {
-//       console.log('done creating categoryseeder!')
-//     })
-// })
-
-// db.once("open", () => {
-//   const categories = [];
-//   categories.push(
-//     Category.create({
-//       name: categoryItem.name,
-//       iconURL: categoryItem.iconURL,
-//       icon: categoryItem.icon
-//     })
-//   );
-//   Promise.all(categories)
-//     .catch(err => ...do something)
-//     .finally(() => db.close())
-// });
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -2,36 +2,19 @@ const db = require('../../config/mongoose')
 const Record = require('../record')
 const dummyrecord = require('./dummyrecord.json').results
 
-// db.once('open', () => {
-//   const records = []
-//   dummyrecord.forEach(recordItem => {
-//     records.push(recordItem)
-//   })
-//   Record.create(records)
-//     .then(() => {
-//       console.log('insert data done...')
-//       return db.close()
-//     })
-//     .then(() => {
-//       console.log('done creating recordseeder!')
-//     })
-
-// })
-
 db.once('open', () => {
   const records = []
-  dummyrecord.forEach(record => {
-    records.push(
-      Record.create({
-        name: record.name,
-        category: record.category,
-        date: record.date,
-        amount: record.amount
-      })
-    )
+  dummyrecord.forEach(recordItem => {
+    records.push(recordItem)
   })
-  console.log('done creating recordseeder!')
-  Promise.all(records)
-    .catch(error => console.log(error))
-    .finally(() => db.close())
+  Record.create(records)
+    .then(() => {
+      console.log('insert data done...')
+      return db.close()
+    })
+    .then(() => {
+      console.log('done creating recordseeder!')
+    })
+
 })
+
