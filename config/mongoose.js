0aa5e40ffe5756002ff3f66899966885421278e8
emily_chen <emilycodeee@gmail.com>
2021-02-07 16:16:25 +0800

    fix:config mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expense-tracker'
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
