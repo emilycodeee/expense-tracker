68a6a42a3be0185bb5af122e1aa2cce33669f66e
Emily Chen <emilycodeee@gmail.com>
2021-12-02 15:15:06 +0800

    Fix image not found

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-// express packages 
-const express = require('express')
-const exphbs = require('express-handlebars')
-const app = express()
-const PORT = process.env.PORT || 3000
+// express packages
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const PORT = process.env.PORT || 3000;
 // require packages used in the project
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 // require routes & config profile
-const routes = require('./routes')
-require('./config/mongoose')
+const routes = require("./routes");
+require("./config/mongoose");
 
 // body parser && method-override
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
 
 // routes setting - Read
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`express is listening on http://localhost:${PORT}`)
-})
+  console.log(`express is listening on http://localhost:${PORT}`);
+});
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,46 +1,48 @@
-const express = require('express')
+const express = require("express");
 
-const Record = require('../../models/record')
-const Category = require('../../models/category')
-const iconGenerator = require('../../Icongenerator')
+const Record = require("../../models/record");
+const Category = require("../../models/category");
+const iconGenerator = require("../../iconGenerator");
 
-const router = express.Router()
+const router = express.Router();
 
 // homepage render
-router.get('/', (req, res) => {
-  const filterItem = req.query.category || ''
-  console.log(filterItem)
-  const selected = {}
+router.get("/", (req, res) => {
+  const filterItem = req.query.category || "";
+  console.log(filterItem);
+  const selected = {};
   if (filterItem) {
-    selected.category = filterItem
+    selected.category = filterItem;
   }
 
-  let totalAmount = 0
-  const categoryItem = []
+  let totalAmount = 0;
+  const categoryItem = [];
 
   // category option
   Category.find()
     .lean()
     .then((item) => {
-      categoryItem.push(...item)
+      categoryItem.push(...item);
 
       // showlist render
       Record.find(selected)
         .lean()
-        .sort({ date: 'asc' })
-        .then(records => {
-          records.forEach(record => {
-            totalAmount += record.amount
-            record.icon = iconGenerator(record.category)
-          })
-          res.render('index', { records, totalAmount, categoryItem, filterItem })
+        .sort({ date: "asc" })
+        .then((records) => {
+          records.forEach((record) => {
+            totalAmount += record.amount;
+            record.icon = iconGenerator(record.category);
+          });
+          res.render("index", {
+            records,
+            totalAmount,
+            categoryItem,
+            filterItem,
+          });
         })
-        .catch(error => console.log(error))
+        .catch((error) => console.log(error));
     })
-    .catch(error => console.log(error))
-})
+    .catch((error) => console.log(error));
+});
 
-
-
-
-module.exports = router
+module.exports = router;
